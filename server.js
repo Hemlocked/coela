95faefad01a4f71d7dbc3aef5c18b30b3e0ea8f0
Hemlocked <phantomofrotten@gmail.com>
2018-12-28 09:20:16 +0900

    テンプレートファイルを作成

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 var http = require('http');
+
 var fs = require('fs');
+
+var pages = require('./static_pages');
+
 var server = http.createServer();
+
+var template = fs.readFileSync(__dirname + '/index.ejs', 'utf-8');
+
+var ejs = require('ejs');
+
+
 server.on ('request', function(req, res) {
-    fs.readFile(__dirname + '/index.html', 'utf-8', function (err, data){
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.write("not found!");
-            return res.end();
-        }
+    var data = ejs.render(template, {
+        content: pages.index
+    });
         res.writeHead(200, {'Content-Type': 'text/html'});
+
         res.write(data);
+
         return res.end();
-    })
-});
+
+    });
+
 server.listen(8000, '127.0.0.1');
+
 console.log("server listening ...");
+
